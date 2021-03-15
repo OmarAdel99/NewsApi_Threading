51e41de9da1086695aa04d05af50199e3817fa39
OmarA <omar3adel14@gmail.com>
2021-03-15 02:02:17 +0200

    Edit db code

--- create_db.py
+++ create_db.py
@@ -2,20 +2,35 @@ import mysql.connector
 
 
 def db_conn():
+    db = mysql.connector.Connect(host="localhost", user="root")
+    cursor = db.cursor()
+    return db, cursor
+
+
+def db_conn2():
     db = mysql.connector.Connect(host="localhost", user="root", db="newsapidb")
     cursor = db.cursor()
     return db, cursor
 
 
-def create_table():
+def create_db():
     db, cursor = db_conn()
     try:
         cursor.execute("CREATE DATABASE newsapidb")
         print("Database newsapidb created.")
+    except Exception as e:
+        print(e)
+    return
+
+
+def create_table():
+    db, cursor = db_conn2()
+    try:
         cursor.execute("CREATE TABLE newsdata (title VARCHAR(255), description VARCHAR(355))")
         print("Table newsdata created.")
     except Exception as e:
         print(e)
 
 
+create_db()
 create_table()
